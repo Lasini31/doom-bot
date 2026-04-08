c890bd540a78842177bcc83edbc9cb97e99d9a04
Lasini31 <lasinidissanayake@gmail.com>
2026-04-08 11:40:54 +0530

    initial commit: manifest setup

---
+++ content.js
@@ -0,0 +1,8 @@
+console.log("Doom Bot: Extension is loaded and watching WhatsApp!");
+
+// This is a helper function to wait for elements to appear
+function waitForElement(selector) {
+    return document.querySelector(selector);
+}
+
+// We will use this later to find the typing area
